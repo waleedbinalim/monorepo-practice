255d4bdc8fb980d8e9548d13912b1b436a2d279e
waleedbinalim <waleedbinalim@gmail.com>
2023-09-09 22:49:31 +0500

    chore: more component segregation

---
+++ apps/client/components/chapter-one/patch-user.tsx
@@ -0,0 +1,55 @@
+import { patchUserAPI } from '@/api';
+import { errMessages } from '@/constants';
+import { handleAPIError } from '@/utils/helpers';
+import { User } from 'common/types';
+import React, { useRef } from 'react';
+
+const patchUser = async (user: User) => {
+  try {
+    const { data } = await patchUserAPI(user);
+    return data;
+  } catch (error) {
+    handleAPIError(error);
+    throw errMessages.unknown;
+  }
+};
+
+type Props = {
+  setUsers: React.Dispatch<React.SetStateAction<User[]>>;
+};
+
+const PatchUserSection: React.FC<Props> = ({ setUsers }) => {
+  const nameRef = useRef<HTMLInputElement>(null);
+  const idRef = useRef<HTMLInputElement>(null);
+
+  return (
+    <>
+      <div className="w-full">
+        <form
+          className="flex flex-col gap-1"
+          onSubmit={async (e) => {
+            e.preventDefault();
+            const id = idRef?.current?.value;
+            const name = nameRef?.current?.value;
+            if (!name || !id) return;
+            const patchedUser = await patchUser({ id, name });
+            setUsers(() => [patchedUser]);
+          }}
+        >
+          <label htmlFor="id">Id:</label>
+          <input className="border-2 px-4 py-2" type="text" ref={idRef} />
+          <label htmlFor="id">name:</label>
+          <input className="border-2 px-4 py-2" type="text" ref={nameRef} />
+          <button
+            className="w-full text-white bg-blue-600 px-4 py-2 rounded-lg"
+            type="submit"
+          >
+            PATCH by Id
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default PatchUserSection;
--- apps/client/pages/chapter-one/index.tsx
+++ apps/client/pages/chapter-one/index.tsx
@@ -1,43 +1,16 @@
-import { getUserByIdAPI, patchUserAPI, postUserAPI } from '@/api';
-import { GetUserByIdSection, GetUsersSection } from '@/components';
-import { errMessages } from '@/constants';
-import { handleAPIError } from '@/utils/helpers';
+import {
+  CreateUserSection,
+  GetUserByIdSection,
+  GetUsersSection,
+  PatchUserSection,
+} from '@/components';
 import { User } from 'common/types';
 import { NextPage } from 'next';
-import { useRef, useState } from 'react';
-
-const getUserById = async (id: string | number) => {
-  try {
-    const { data } = await getUserByIdAPI(id);
-    return data;
-  } catch (error) {
-    handleAPIError(error);
-    throw errMessages.unknown;
-  }
-};
-
-const postUsers = async (name: string) => {
-  try {
-    const { data } = await postUserAPI({ name });
-    return data;
-  } catch (error) {
-    throw errMessages.unknown;
-  }
-};
-
-const patchUser = async (user: User) => {
-  try {
-    const { data } = await patchUserAPI(user);
-    return data;
-  } catch (error) {
-    throw errMessages.unknown;
-  }
-};
+import { useState } from 'react';
 
 const ChapterOnePage: NextPage = () => {
   const [users, setUsers] = useState<User[]>([]);
-  const nameRef = useRef<HTMLInputElement>(null);
-  const idRef = useRef<HTMLInputElement>(null);
+
   return (
     <>
       <div className="flex flex-col align-middle justify-center items-center">
@@ -47,61 +20,8 @@ const ChapterOnePage: NextPage = () => {
           <div className="flex flex-col gap-4 w-1/2 px-4 py-2">
             <GetUsersSection setUsers={setUsers} />
             <GetUserByIdSection setUsers={setUsers} />
-
-            <div className="w-full">
-              <form
-                className="flex flex-col gap-1"
-                onSubmit={async (e) => {
-                  e.preventDefault();
-                  const name = nameRef?.current?.value;
-                  if (!name) return;
-                  const newUser = await postUsers(name);
-                  setUsers((current) => [...current, newUser]);
-                }}
-              >
-                <label htmlFor="id">Name:</label>
-                <input
-                  className="border-2 px-4 py-2"
-                  type="text"
-                  ref={nameRef}
-                />
-                <button
-                  className="w-full text-white bg-blue-600 px-4 py-2 rounded-lg"
-                  type="submit"
-                >
-                  POST
-                </button>
-              </form>
-            </div>
-
-            <div className="w-full">
-              <form
-                className="flex flex-col gap-1"
-                onSubmit={async (e) => {
-                  e.preventDefault();
-                  const id = idRef?.current?.value;
-                  const name = nameRef?.current?.value;
-                  if (!name || !id) return;
-                  const patchedUser = await patchUser({ id, name });
-                  setUsers(() => [patchedUser]);
-                }}
-              >
-                <label htmlFor="id">Id:</label>
-                <input className="border-2 px-4 py-2" type="text" ref={idRef} />
-                <label htmlFor="id">name:</label>
-                <input
-                  className="border-2 px-4 py-2"
-                  type="text"
-                  ref={nameRef}
-                />
-                <button
-                  className="w-full text-white bg-blue-600 px-4 py-2 rounded-lg"
-                  type="submit"
-                >
-                  PATCH by Id
-                </button>
-              </form>
-            </div>
+            <CreateUserSection setUsers={setUsers} />
+            <PatchUserSection setUsers={setUsers} />
           </div>
 
           <div className="border-2 rounded-lg w-1/2 px-4 py-2 font-semibold">
