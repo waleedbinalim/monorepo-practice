b5dec0604f4882a9c977346e4871bb8af748c6f9
waleedbinalim <waleedbinalim@gmail.com>
2023-09-10 23:32:02 +0500

    ui imporved

--- apps/client/pages/chapter-two/login/index.tsx
+++ apps/client/pages/chapter-two/login/index.tsx
@@ -7,31 +7,46 @@ const ChapterTwoLoginPage: NextPage = () => {
   const passwordRef = useRef<HTMLInputElement>(null);
   return (
     <>
-      <form
-        className="flex flex-col gap-2"
-        onSubmit={async (e) => {
-          e.preventDefault();
-          const username = usernameRef?.current?.value;
-          const password = passwordRef?.current?.value;
+      <div className="w-full min-h-screen bg-gray-800 bg-opacity-50 flex justify-center items-center">
+        <div className="w-1/2 p-8 bg-white rounded-lg ">
+          <form
+            className="flex flex-col gap-2"
+            onSubmit={async (e) => {
+              e.preventDefault();
+              const username = usernameRef?.current?.value;
+              const password = passwordRef?.current?.value;
 
-          if (!username || !password) return;
+              if (!username || !password) return;
 
-          try {
-            const { data } = await signinUserAPI({ username, password });
-            console.log(data);
-          } catch (error) {
-            console.log(error);
-          }
-        }}
-      >
-        <label>username</label>
-        <input className="border-2 px-4 py-2 w-1/2" ref={usernameRef} />
-        <label>password</label>
-        <input className="border-2 px-4 py-2 w-1/2" ref={passwordRef} />
-        <button className="px-4 py-2 w-1/2 bg-blue-600" type="submit">
-          Submit
-        </button>
-      </form>
+              try {
+                const { data } = await signinUserAPI({ username, password });
+                console.log(data);
+              } catch (error) {
+                console.log(error);
+              }
+            }}
+          >
+            <label className="text-sm capitalize">Username:</label>
+            <input
+              className="border-2 px-4 py-2 w-100 rounded-lg"
+              ref={usernameRef}
+            />
+            <label className="text-sm capitalize">Password:</label>
+            <input
+              className="border-2 px-4 py-2 w-100 rounded-lg"
+              ref={passwordRef}
+            />
+            <div className="w-100 flex justify-center mt-2">
+              <button
+                className="px-4 py-2 w-1/2 bg-blue-600 rounded-lg text-white"
+                type="submit"
+              >
+                Submit
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
     </>
   );
 };
