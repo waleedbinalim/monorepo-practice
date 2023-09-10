cc29d1ce1cff3b06abebdf58e5ca7fef4ba334e6
waleedbinalim <waleedbinalim@gmail.com>
2023-09-10 11:45:02 +0500

    chore: pages added for chapter-two

--- apps/client/constants/routes.ts
+++ apps/client/constants/routes.ts
@@ -1,3 +1,5 @@
 export const pageRoutes = {
   chapterOne: '/chapter-one',
+  chapterTwo: '/chapter-two',
+  chapterTwoLogin: '/chapter-two/login',
 };
--- apps/client/pages/index.tsx
+++ apps/client/pages/index.tsx
@@ -1,23 +1,32 @@
 import { NextPage } from 'next';
 import Link from 'next/link';
-import { useRouter } from 'next/router';
 import { pageRoutes } from '../constants';
 
+const routeAndText: { route: string; text: string }[] = [
+  { route: pageRoutes.chapterOne, text: 'Chapter One' },
+  { route: pageRoutes.chapterTwo, text: 'Chapter Two' },
+  { route: pageRoutes.chapterTwoLogin, text: 'Chapter Two Login' },
+];
+
 export const HomePage: NextPage = () => {
-  const router = useRouter();
   return (
-    <div>
+    <div className="px-4 py-2">
       <button className="min-w-[12px] font-bold px-4 py-2 rounded bg-blue-400">
         This is the Hoooommee page
       </button>
 
-      <div>
+      <div className="flex flex-col">
         Go to{' '}
-        <Link href={pageRoutes.chapterOne}>
-          <span className="text-blue-500 underline cursor-pointer">
-            Chapter One
-          </span>
-        </Link>
+        {routeAndText?.map((item) => {
+          const { route, text } = item;
+          return (
+            <Link href={route} key={route}>
+              <span className="text-blue-500 underline cursor-pointer">
+                {text}
+              </span>
+            </Link>
+          );
+        })}
       </div>
     </div>
   );
